b93daa3a2845c4f0908c001559c856c83e276878
Lucas <lucasmarinozipitta@gmail.com>
2024-03-07 17:44:31 -0300

    lista1

---
+++ EX1.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+	 float r1, r2 , r3, req;
+	 
+	 printf("informe a resistencia 1:");
+	 scanf("%f", &r1);
+	  printf("informe a resistencia 2:");
+	 scanf("%f", &r2);
+	  printf("informe a resistencia 3:");
+	 scanf("%f", &r3);
+	 
+	 req = r1 + r2 + r3;
+	 
+	 printf("a req e de %f", req);
+	 
+	
+	
+	
+	
+	
+	
+	
+return 0;	
+}
